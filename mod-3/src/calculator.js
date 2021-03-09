14df56c39960a8762580424005934f09f62b9e11
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-03-09 17:20:33 +0100

    React Tooling

--- mod-3/src/calculator.js
+++ mod-3/src/calculator.js
@@ -1,5 +1,5 @@
-class Calculator {
-  add(x, y) {
+export class Calculator {
+  static add(x, y) {
     return x + y;
   }
 }
--- mod-3/src/main.js
+++ mod-3/src/main.js
@@ -1,4 +1,6 @@
+import { Calculator } from './calculator';
+
 const calc = new Calculator();
 
 const root = document.getElementById('root');
-root.textContent = `1 + 2 = ${calc.add(1, 2)}`;
+root.textContent = `1 + 5 = ${calc.add(1, 5)}`;
