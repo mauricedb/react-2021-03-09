24fde57ca9549ffc51690855c968b12269634f1e
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-03-09 13:56:04 +0100

    Before tooling

---
+++ mod-3/src/greeting.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Greeting() {
+  return <h1>Greetings</h1>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
---
+++ mod-3/src/main.js
@@ -0,0 +1,4 @@
+const calc = new Calculator();
+
+const root = document.getElementById('root');
+root.textContent = `1 + 2 = ${calc.add(1, 2)}`;
