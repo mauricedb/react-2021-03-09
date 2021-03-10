04b00f00d19c794194bf0c691d8438913241deb8
Maurice de Beijer <maurice.de.beijer@gmail.com>
2021-03-10 10:24:57 +0100

    Before data entry

---
+++ mod-5/src/App.js
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { UncontrolledPersonForm } from './components/uncontrolledPersonForm';
+import { ControlledPersonForm } from './components/controlledPersonForm';
+
+const initialPerson = {
+  id: 1,
+  firstname: 'Spears',
+  surname: 'Mcmahon',
+  email: 'spearsmcmahon@comtract.com',
+  balance: 3_213.97,
+  phone: '(865) 535-2756',
+  address: '674 Moultrie Street\nHobucken\nMaryland',
+};
+
+function App() {
+  return (
+    <div className="container">
+      <UncontrolledPersonForm initialPerson={initialPerson} />
+      {/* <ControlledPersonForm initialPerson={initialPerson} /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ mod-5/src/components/uncontrolledPersonForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export function UncontrolledPersonForm({ initialPerson }) {
+  const [person, setPerson] = React.useState(initialPerson);
+
+  return (
+    <form>
+      <div className="mb-3">
+        <label className="form-label">Firstname</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Surname</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Address</label>
+        <textarea className="form-control" rows={3} />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Email address</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Phone</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Balance</label>
+        <input type="text" className="form-control" />
+      </div>
+      <button className="btn btn-primary">Save</button>
+    </form>
+  );
+}
